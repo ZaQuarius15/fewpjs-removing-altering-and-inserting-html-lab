2b32163f50871e856042518b096134de497e2807
ZaQuarius15 <jzcarlson014@gmail.com>
2020-10-26 12:04:22 -0500

    commit tree 92fb62611efd593ad0792dcff7021a176f8f3d63

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector("main").remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Z is the champion!";
+newHeader.className = "vicotry";
